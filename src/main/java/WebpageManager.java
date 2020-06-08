ee49f88311762037f93a0dee8cab931a026e70b6
Callum Newlands <callumnewlands@googlemail.com>
2020-06-08 22:44:16 +0100

    Replaces java.net code with JSoup

--- src/main/java/WebpageManager.java
+++ src/main/java/WebpageManager.java
@@ -1,19 +1,11 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class WebpageManager {
 
 	public static void main(String[] args) throws IOException {
-		// Doesn't follow redirects from http to https and v.v.
-		URL oracle = new URL("https://www.oracle.com/");
-		BufferedReader in = new BufferedReader(
-				new InputStreamReader(oracle.openStream()));
-
-		String inputLine;
-		while ((inputLine = in.readLine()) != null)
-			System.out.println(inputLine);
-		in.close();
+		Document doc = Jsoup.connect("http://oracle.org/").get();
+		System.out.println(doc);
 	}
 }
