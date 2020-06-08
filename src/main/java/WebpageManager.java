1c262ba23b33a42f996d9c7ae3e2fd88c3975a1f
Callum Newlands <callumnewlands@googlemail.com>
2020-06-08 22:41:09 +0100

    Adds example code to read from URL

--- src/main/java/WebpageManager.java
+++ src/main/java/WebpageManager.java
@@ -1,6 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public class WebpageManager {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		// Doesn't follow redirects from http to https and v.v.
+		URL oracle = new URL("https://www.oracle.com/");
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(oracle.openStream()));
 
+		String inputLine;
+		while ((inputLine = in.readLine()) != null)
+			System.out.println(inputLine);
+		in.close();
 	}
 }
