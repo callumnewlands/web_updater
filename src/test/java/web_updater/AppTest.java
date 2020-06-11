31e40a894174f1573ce416e7cce6763de68ebe15
Callum Newlands <callumnewlands@googlemail.com>
2020-06-11 17:47:29 +0100

    Adds CI testing workflow

---
+++ src/test/java/web_updater/AppTest.java
@@ -0,0 +1,14 @@
+package web_updater;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void testTrue() {
+		assertTrue(true);
+	}
+
+}
