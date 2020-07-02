54c96851e2c12f446fb202af4b43c6b64c61826e
Callum Newlands <callumnewlands@googlemail.com>
2020-07-02 21:57:30 +0100

    Renames user table to avoid conflict with Heroku user table

--- src/main/java/web_updater/security/User.java
+++ src/main/java/web_updater/security/User.java
@@ -5,8 +5,10 @@ import javax.persistence.Convert;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "app_user")
 public class User {
 
 	@Id
